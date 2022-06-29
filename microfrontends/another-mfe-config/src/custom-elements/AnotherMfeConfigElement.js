65d52c457dfcc1b5111f40c34ba67e673292a79b
Jeff Go <jgo@entando.com>
2022-06-29 22:31:52 +0800

    ENG-3881 implement public path for remote MFE asset files

--- microfrontends/another-mfe-config/src/custom-elements/AnotherMfeConfigElement.js
+++ microfrontends/another-mfe-config/src/custom-elements/AnotherMfeConfigElement.js
@@ -1,3 +1,4 @@
+import './public-path';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AnotherMfeConfig from '../AnotherMfeConfig';
---
+++ microfrontends/jeff-mfe/src/custom-elements/public-path.js
@@ -0,0 +1,4 @@
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line no-undef
+  __webpack_public_path__ = window.entandoMFEPublicPaths['jeff-mfe'];
+}
