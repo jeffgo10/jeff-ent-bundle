f3ccb5deb7d081e9ca49cee42a6a380a673dcfcc
zonia3000 <s.zorba@entando.com>
2022-07-06 10:57:15 +0200

    another-ms: removed authentication from endpoints not starting with /api

--- microservices/another-ms/src/main/java/com/example/demo/security/SecurityConfig.java
+++ microservices/another-ms/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .mvcMatchers("/api/**").hasRole("USER")
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer().jwt()
                 .jwtAuthenticationConverter(jwtAuthenticationConverter())
