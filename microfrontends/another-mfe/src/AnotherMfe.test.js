f1478ccf74c12b5754615ab465592b12da45f0e3
Rax Canaan Layumas <raxkaynan@gmail.com>
2022-07-08 14:43:18 +0800

    Add more tests for AnotherMfe

--- microfrontends/another-mfe/src/AnotherMfe.test.js
+++ microfrontends/another-mfe/src/AnotherMfe.test.js
@@ -10,23 +10,15 @@ const keycloak = {
   login: jest.fn()
 };
 
-const config = {
-  systemParams: {
-    api: {
-      'int-api': { url: 'http://localhost:8082' },
-      'ext-api': { url: 'http://test-fire.apps.mainline.eng-entando.com/cecchisandrone/simple-ms' }
-    }
-  },
-  contextParams: {
-    page_code: 'my_mfe_page'
-  },
-  params: {
-    username: 'adionisi',
-    description: 'Tech Lead Entando'
-  }
-};
-
 test('displays fetched timestamps when "Get timestamps" button is clicked', async () => {
+  const config = {
+    systemParams: {
+      api: {
+        'int-api': { url: 'http://localhost:8082' },
+        'ext-api': { url: 'http://test-fire.apps.mainline.eng-entando.com/cecchisandrone/simple-ms' }
+      }
+    },
+  };
   const internalTimestamp = 'Jul 07, 2022 19:00';
   const externalTimestamp = 'Jul 07, 2022 11:00';
 
@@ -51,6 +43,40 @@ test('displays fetched timestamps when "Get timestamps" button is clicked', asyn
   const getTimestampsBtnEl = screen.getByText(/Get timestamps/i);
   userEvent.click(getTimestampsBtnEl);
 
-  expect(await screen.findByText(`Internal timestamp: ${internalTimestamp}`)).toBeInTheDocument();
-  expect(await screen.findByText(`External timestamp: ${externalTimestamp}`)).toBeInTheDocument();
+  expect(await screen.findByText(new RegExp(internalTimestamp))).toBeInTheDocument();
+  expect(await screen.findByText(new RegExp(externalTimestamp))).toBeInTheDocument();
+});
+
+test('displays page code', async () => {
+  const config = {
+    contextParams: {
+      page_code: 'my_mfe_page'
+    }
+  };
+
+  render(
+    <KeycloakContext.Provider value={keycloak}>
+      <AnotherMfe config={config} />
+    </KeycloakContext.Provider>
+  );
+
+  expect(screen.getByText(new RegExp(config.contextParams.page_code))).toBeInTheDocument();
+});
+
+test('displays username and description', async () => {
+  const config = {
+    params: {
+      username: 'adionisi',
+      description: 'Tech Lead Entando'
+    }
+  };
+
+  render(
+    <KeycloakContext.Provider value={keycloak}>
+      <AnotherMfe config={config} />
+    </KeycloakContext.Provider>
+  );
+
+  expect(screen.getByText(new RegExp(config.params.username))).toBeInTheDocument();
+  expect(screen.getByText(new RegExp(config.params.description))).toBeInTheDocument();
 });
