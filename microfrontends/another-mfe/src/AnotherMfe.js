d13f607fb3cbd31e922861da731ed1f5e4e7279d
Jeff Go <j.go@entando.com>
2022-07-14 15:35:57 +0800

    reverted public path for another-mfe

--- microfrontends/another-mfe/src/AnotherMfe.js
+++ microfrontends/another-mfe/src/AnotherMfe.js
@@ -1,7 +1,7 @@
 import { useContext, useState } from 'react';
+import logo from './react-logo.svg';
 import './AnotherMfe.css';
 import KeycloakContext from './KeycloakContext';
-import ReactLogo from './ReactLogo';
 
 const API_TIMESTAMP_PATH = '/api/timestamp'
 
@@ -70,7 +70,7 @@ function AnotherMfe({ config }) {
   return (
     <div className="App">
       <header className="App-header">
-        <ReactLogo className="App-logo" />
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
       <button onClick={handleGetTimestampsClick}>Get timestamps</button>
       {internalTimestamp && (
